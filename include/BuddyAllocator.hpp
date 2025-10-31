9780bd0ed15c62d7219ac543cd88f0bab8050c59
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2025-10-31 14:00:00 -0400

    Add free list structure + initial buddy layout

--- include/BuddyAllocator.hpp
+++ include/BuddyAllocator.hpp
@@ -2,6 +2,7 @@
 #define TENSORPOOL_BUDDY_ALLOCATOR_HPP_
 
 #include <cstddef>
+#include <list>
 
 class BuddyAllocator {
  public:
@@ -11,9 +12,18 @@ class BuddyAllocator {
   void Release(void* ptr, size_t bytes);
 
  private:
+  struct Block {
+    void* ptr;
+    size_t size;
+  };
+
+  static constexpr int kMaxLevels = 32;
+  std::list<Block> free_lists_[kMaxLevels];
+
   void* base_;
   size_t total_bytes_;
   size_t alignment_;
+  int max_level_;
 };
 
 #endif  // TENSORPOOL_BUDDY_ALLOCATOR_HPP_
--- src/BuddyAllocator.cpp
+++ src/BuddyAllocator.cpp
@@ -5,13 +5,20 @@ BuddyAllocator::BuddyAllocator(void* base,
                                size_t alignment)
     : base_(base),
       total_bytes_(total_bytes),
-      alignment_(alignment) {}
+      alignment_(alignment),
+      max_level_(0) {
+  while ((1ULL << max_level_) < total_bytes_) {
+    max_level_++;
+  }
+  max_level_--;
+
+  free_lists_[max_level_].push_back({base_, total_bytes_});
+}
 
 void* BuddyAllocator::Allocate(size_t bytes) {
-  // stub for now
-  return nullptr;
+  return nullptr;  // not implemented yet
 }
 
 void BuddyAllocator::Release(void* ptr, size_t bytes) {
-  // stub
+  // noop for now
 }
