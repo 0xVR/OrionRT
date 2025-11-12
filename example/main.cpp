742793cdc3ac01fa25f138c47cbc92bad20942df
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2025-11-12 14:00:00 -0500

    Add example main program demonstrating TensorPool usage

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+#include "TensorPool.hpp"
+
 int main() {
-    std::cout << "Hello TensorPool (WIP)" << std::endl;
-    return 0;
+  TensorPool pool(64 * 1024 * 1024);  // 64 MB arena
+
+  std::vector<OpNode> ops = {
+      {0, {}, {0}},
+      {1, {0}, {1}},
+      {2, {1}, {2}},
+      {3, {2}, {}},
+  };
+
+  std::vector<TensorDesc> descs = {
+      {0, 1024},
+      {1, 2048},
+      {2, 1024},
+  };
+
+  pool.RegisterGraph(ops, descs);
+  pool.FinalizePlan();
+
+  for (int i = 0; i < 3; i++) {
+    auto h = pool.Allocate(i);
+    std::cout << "Tensor " << i << " → ptr=" << h.ptr
+              << " bytes=" << h.bytes << std::endl;
+  }
+
+  std::cout << "Peak memory = " << pool.peak_memory() << " bytes\n";
 }
